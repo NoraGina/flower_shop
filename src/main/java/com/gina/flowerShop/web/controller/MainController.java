0da4862f1af9941df54df403aa8a9d07d408d2cf
Gina <gina.agapescu@gmail.com>
2021-03-06 20:06:51 +0200

    Create MainController class

---
+++ src/main/java/com/gina/flowerShop/web/controller/MainController.java
@@ -0,0 +1,42 @@
+package com.gina.flowerShop.web.controller;
+
+import com.gina.flowerShop.service.UserService;
+import com.gina.flowerShop.web.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        //model.addAttribute("products", productService.findAll());
+        
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(@AuthenticationPrincipal UserDetails currentUser, Model model ){
+        // SecurityContext context = SecurityContextHolder.getContext();
+
+        // model.addAttribute("message", "You are logged in as "
+        //         + context.getAuthentication().getName());
+        UserDto user= userService.findByUsername(currentUser.getUsername());
+        model.addAttribute("currentUser", user);
+
+        return "dashboard";
+    }
+
+}
