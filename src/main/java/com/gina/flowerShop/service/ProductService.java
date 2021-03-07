87f209d92d9244d3119e2accbc25ab2fbbc0190a
Gina <gina.agapescu@gmail.com>
2021-03-07 07:22:17 +0200

    Create ProductService interface, crud methods, and all methods from ProductRepository

---
+++ src/main/java/com/gina/flowerShop/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.gina.flowerShop.service;
+
+import com.gina.flowerShop.web.dto.ProductDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<ProductDto> findAll();
+    Optional<ProductDto> findOne(Long idProduct);
+    void save(ProductDto productDto);
+    void delete(Long idProduct);
+    List<String>findDistinctOrigin();
+    List<ProductDto> findAllByCategoryId(Long idCategory);
+    List<ProductDto>findAllByStockGreaterThan( int stock);
+    List<ProductDto> findAllByCategoryIdAndStockGreaterThan(Long idCategory, int stock);
+}
