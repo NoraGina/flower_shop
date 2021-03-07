97ec966d4be73147fd472a20967ced37bddcb263
Gina <gina.agapescu@gmail.com>
2021-03-07 09:13:02 +0200

    Create class ShippingAddress and map customer-shippingAddresses

--- src/main/java/com/gina/flowerShop/model/Customer.java
+++ src/main/java/com/gina/flowerShop/model/Customer.java
@@ -4,10 +4,21 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import java.util.Set;
 
 @Entity
 @Table(name = "customers")
 public class Customer extends User {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private Set<ShippingAddress> shippingAddresses;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String fullName, String username, @Email String email, String phone, String password,
+                    Set<Role> roles, Set<ShippingAddress> shippingAddresses) {
+        super(id, fullName, username, email, phone, password, roles);
+        this.shippingAddresses = shippingAddresses;
+    }
 }
