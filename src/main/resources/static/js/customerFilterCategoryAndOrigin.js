0b4a808e2e03d3007c41435e6c4b9adf22220c5b
Gina Agapescu <gina.agapescu@gmail.com>
2022-01-20 19:18:41 +0200

    add filter products by category and origin javascript

---
+++ src/main/resources/static/js/customerFilterCategoryAndOrigin.js
@@ -0,0 +1,35 @@
+  /* TOGGLE  NAVBAR Filtred Page HAMBURGER */
+      const toggleButton = document.querySelector('#toggleBtn');
+      const naviList = document.querySelector('#navListId');
+      const userDivId = document.querySelector('#customerDivId')
+       toggleButton.addEventListener('click', () => {
+            naviList.classList.toggle('active');
+            userDivId.classList.toggle('active');
+        })
+
+        /* TOGGLE FILTER NAVBAR Filtred Page HAMBURGER */
+      const filterToggleButton=document.querySelector('#toggleFilterBtn');
+      const navFilterList = document.querySelector('#navFilterListId');
+      filterToggleButton.addEventListener('click', ()=>{
+          navFilterList.classList.toggle('filterActive');
+      })
+
+      /*Function to open and close Modal*/
+    function openMyModal(){
+       let modals = document.querySelectorAll('.modal.myModal')
+              // Get the button that opens the modal
+        let btns = document.querySelectorAll('.openmodal');
+        let spans = document.querySelectorAll(".close");
+            for (let i = 0; i < btns.length; i++) {
+                btns[i].onclick = function () {
+                    modals[i].classList.remove('closeModal');
+                    modals[i].classList.add('showModal')
+                }
+            }
+            for (let i = 0; i < spans.length; i++) {
+                spans[i].onclick = function () {
+                    modals[i].classList.remove('showModal');
+                    modals[i].classList.add('closeModal');
+                }
+            }
+    }
